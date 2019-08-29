2ff8b47367488647dbf9a2fc9b5c6ea6748c4e0c
Edison Wu <ewu@digitalriver.com>
2019-08-29 18:04:31 +0800

    update travis yml

---
+++ index-staging-test.js
@@ -0,0 +1,9 @@
+import {Selector} from 'testcafe';
+
+fixture`CI/CD testing`
+  .page`https://ci-test-staging.herokuapp.com/`;
+
+test('page test', async t => {
+  await t
+    .expect(Selector('#page-title').innerText).eql('Hello, Travis-CI & Heroku!');
+});
